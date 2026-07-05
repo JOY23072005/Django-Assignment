9045fde23766efd1de7c57db57ff1027d5359186
JOY23072005 <hansjoydeep@gmail.com>
2026-07-05 19:14:55 +0530

    feat(patients): define patient schema with demographic and medical profile fields

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from common.models import TimeStampedModel
 
-# Create your models here.
+
+class Patient(TimeStampedModel):
+
+    class Gender(models.TextChoices):
+        MALE = "Male", "Male"
+        FEMALE = "Female", "Female"
+        OTHER = "Other", "Other"
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="patients"
+    )
+
+    name = models.CharField(max_length=150)
+
+    age = models.PositiveIntegerField()
+
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices
+    )
+
+    phone = models.CharField(max_length=15)
+
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
